439f6f85bbe1946e58a41e5733e1c9ce9b35b428
Tran Toan <trantoan161238@gmail.com>
2024-03-31 12:25:32 +0700

    fix main

--- index.js
+++ index.js
@@ -153,8 +153,12 @@ async function startServer() {
     }
     await user.save();
     console.log("Database Connect Successfully!");
-    app.listen(3000, () => {
-      console.log("Server are listen at port: http://localhost:3000");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        "Express server listening on port %d in %s mode",
+        this.address().port,
+        app.settings.env
+      );
     });
   } catch (err) {
     console.log(err);
