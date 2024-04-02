ad8d3355a05e500e28f86464e26a24d99f9a2efd
Tran Toan <trantoan161238@gmail.com>
2024-04-02 09:38:05 +0700

    updated

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-const express = require("express");
-const path = require("path");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const mongoose = require("mongoose");
-const csurf = require("csurf");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-const helmet = require("helmet");
-const compression = require("compression");
-const rateLimit = require("express-rate-limit");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const mongoose = require('mongoose');
+const csurf = require('csurf');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const helmet = require('helmet');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+require('dotenv').config();
 
 // Routes
-const authRoutes = require("./routes/auth/auth");
-const adminRoutes = require("./routes/admin/admin");
-const userRoutes = require("./routes/user/user");
+const authRoutes = require('./routes/auth/auth');
+const adminRoutes = require('./routes/admin/admin');
+const userRoutes = require('./routes/user/user');
 
 const MONGO_URI =
-  "mongodb+srv://teamcode:teamcode@code.1gxvbjo.mongodb.net/nft_game";
+  'mongodb+srv://teamcode:teamcode@code.1gxvbjo.mongodb.net/nft_game';
 
-const User = require("./models/user");
+const User = require('./models/user');
 
 const limiter = rateLimit({
   // Limit requests (100 req / 3 mins) per user.
   windowMs: 1 * 60 * 1000,
   max: 1000,
-  message: "Too many requests. Please try again in a few minutes.",
+  message: 'Too many requests. Please try again in a few minutes.',
 });
 
 const store = new MongoDBStore({
   uri: MONGO_URI,
-  collection: "sessions",
+  collection: 'sessions',
 });
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(helmet());
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      "img-src": ["'self'", "*"],
-      "media-src": ["'self'", "*"],
-      "script-src": ["'self'", "*"],
-      "frame-src": ["'self'", "*"],
+      'img-src': ["'self'", '*'],
+      'media-src': ["'self'", '*'],
+      'script-src': ["'self'", '*', "'unsafe-inline'", "'unsafe-eval'"],
+      'frame-src': ["'self'", '*'],
     },
   })
 );
@@ -56,7 +56,7 @@ app.use(cors());
 app.use(compression());
 app.use(limiter);
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
@@ -97,20 +97,20 @@ app.use(authRoutes);
 app.use(userRoutes);
 
 // Route for admin
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 
 // 404 Error Middleware
 app.use((req, res, next) => {
-  return res.render("error/404", {
-    title: "404 not found",
+  return res.render('error/404', {
+    title: '404 not found',
   });
 });
 
 // 500 Server Error middleware
 app.use((error, req, res, next) => {
   console.log(error);
-  res.render("error/500", {
-    title: "500 Server Error",
+  res.render('error/500', {
+    title: '500 Server Error',
     errorMessage: error.message,
   });
 });
@@ -127,7 +127,7 @@ async function startServer() {
       });
     }
     await user.save();
-    console.log("Database Connect Successfully!");
+    console.log('Database Connect Successfully!');
     app.listen(process.env.PORT || 3000);
   } catch (err) {
     console.log(err);
